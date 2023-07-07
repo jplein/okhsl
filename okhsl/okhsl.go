26559df1b51bf1974e46739ec7656daeea6fea04
Jason Plein <jplein@gmail.com>
2023-07-07 12:27:33 -0700

    Internal types and fields lowercase

--- okhsl/okhsl.go
+++ okhsl/okhsl.go
@@ -8,23 +8,23 @@ import (
 
 // Data types
 
-type Lab struct {
-	L float64
+type lab struct {
+	l float64
 	a float64
 	b float64
 }
-type Cs struct {
-	C_0   float64
-	C_mid float64
-	C_max float64
+type cs struct {
+	c_0   float64
+	c_mid float64
+	c_max float64
 }
-type LC struct {
-	L float64
-	C float64
+type lc struct {
+	l float64
+	c float64
 }
-type ST struct {
-	S float64
-	T float64
+type st struct {
+	s float64
+	t float64
 }
 
 // Private functions
@@ -108,10 +108,10 @@ func compute_max_saturation(a, b float64) float64 {
 	return S
 }
 
-func oklab_to_linear_srgb(c Lab) types.RGB {
-	l_ := c.L + 0.3963377774*c.a + 0.2158037573*c.b
-	m_ := c.L - 0.1055613458*c.a - 0.0638541728*c.b
-	s_ := c.L - 0.0894841775*c.a - 1.2914855480*c.b
+func oklab_to_linear_srgb(c lab) types.RGB {
+	l_ := c.l + 0.3963377774*c.a + 0.2158037573*c.b
+	m_ := c.l - 0.1055613458*c.a - 0.0638541728*c.b
+	s_ := c.l - 0.0894841775*c.a - 1.2914855480*c.b
 
 	l := l_ * l_ * l_
 	m := m_ * m_ * m_
@@ -125,16 +125,16 @@ func oklab_to_linear_srgb(c Lab) types.RGB {
 	return rgb
 }
 
-func find_cusp(a, b float64) LC {
+func find_cusp(a, b float64) lc {
 	// First, find the maximum saturation (saturation S = C/L)
 	S_cusp := compute_max_saturation(a, b)
 
 	// Convert to linear sRGB to find the first point where at least one of r,g or b >= 1:
-	rgb_at_max := oklab_to_linear_srgb(Lab{1, S_cusp * a, S_cusp * b})
+	rgb_at_max := oklab_to_linear_srgb(lab{1, S_cusp * a, S_cusp * b})
 	L_cusp := math.Cbrt(1.0 / math.Max(math.Max(rgb_at_max.R, rgb_at_max.G), rgb_at_max.B))
 	C_cusp := L_cusp * S_cusp
 
-	return LC{L_cusp, C_cusp}
+	return lc{L_cusp, C_cusp}
 }
 
 // Finds intersection of the line defined by
@@ -147,15 +147,15 @@ func find_gamut_intersection(a, b, L1, C1, L0 float64) float64 {
 
 	// Find the intersection for upper and lower half seprately
 	var t float64
-	if ((L1-L0)*cusp.C - (cusp.L-L0)*C1) <= 0. {
+	if ((L1-L0)*cusp.c - (cusp.l-L0)*C1) <= 0. {
 		// Lower half
 
-		t = cusp.C * L0 / (C1*cusp.L + cusp.C*(L0-L1))
+		t = cusp.c * L0 / (C1*cusp.l + cusp.c*(L0-L1))
 	} else {
 		// Upper half
 
 		// First intersect with triangle
-		t = cusp.C * (L0 - 1.) / (C1*(cusp.L-1.) + cusp.C*(L0-L1))
+		t = cusp.c * (L0 - 1.) / (C1*(cusp.l-1.) + cusp.c*(L0-L1))
 
 		// Then one step Halley's method
 		{
@@ -235,13 +235,13 @@ func find_gamut_intersection(a, b, L1, C1, L0 float64) float64 {
 	return t
 }
 
-func to_ST(cusp LC) ST {
-	L := cusp.L
-	C := cusp.C
-	return ST{C / L, C / (1 - L)}
+func to_ST(cusp lc) st {
+	L := cusp.l
+	C := cusp.c
+	return st{C / L, C / (1 - L)}
 }
 
-func get_ST_mid(a_, b_ float64) ST {
+func get_ST_mid(a_, b_ float64) st {
 	S := 0.11516993 + 1./(+7.44778970+4.15901240*b_+
 		a_*(-2.19557347+1.75198401*b_+
 			a_*(-2.13704948-10.02301043*b_+
@@ -252,25 +252,25 @@ func get_ST_mid(a_, b_ float64) ST {
 			a_*(-0.27087943+0.61223990*b_+
 				a_*(+0.00299215-0.45399568*b_-0.14661872*a_))))
 
-	return ST{S, T}
+	return st{S, T}
 }
 
-func get_Cs(L, a_, b_ float64) Cs {
+func get_Cs(L, a_, b_ float64) cs {
 	cusp := find_cusp(a_, b_)
 
 	C_max := find_gamut_intersection(a_, b_, L, 1, L /* cusp */)
 	ST_max := to_ST(cusp)
 
 	// Scale factor to compensate for the curved part of gamut shape:
-	k := C_max / math.Min((L*ST_max.S), (1-L)*ST_max.T)
+	k := C_max / math.Min((L*ST_max.s), (1-L)*ST_max.t)
 
 	var C_mid float64
 	{
 		ST_mid := get_ST_mid(a_, b_)
 
 		// Use a soft minimum function, instead of a sharp triangle shape to get a smooth value for chroma.
-		C_a := L * ST_mid.S
-		C_b := (1.0 - L) * ST_mid.T
+		C_a := L * ST_mid.s
+		C_b := (1.0 - L) * ST_mid.t
 		C_mid = 0.9 * k * math.Sqrt(math.Sqrt(1.0/(1.0/(C_a*C_a*C_a*C_a)+1.0/(C_b*C_b*C_b*C_b))))
 	}
 
@@ -284,7 +284,7 @@ func get_Cs(L, a_, b_ float64) Cs {
 		C_0 = math.Sqrt(1.0 / (1.0/(C_a*C_a) + 1.0/(C_b*C_b)))
 	}
 
-	return Cs{C_0: C_0, C_mid: C_mid, C_max: C_max}
+	return cs{c_0: C_0, c_mid: C_mid, c_max: C_max}
 }
 
 func toe_inv(x float64) float64 {
@@ -325,9 +325,9 @@ func Okhsl_to_srgb(hsl types.HSL) types.RGB {
 	L := toe_inv(l)
 
 	cs := get_Cs(L, a_, b_)
-	C_0 := cs.C_0
-	C_mid := cs.C_mid
-	C_max := cs.C_max
+	C_0 := cs.c_0
+	C_mid := cs.c_mid
+	C_max := cs.c_max
 
 	// Interpolate the three values for C so that:
 	// At s=0: dC/ds = C_0, C=0
@@ -356,7 +356,7 @@ func Okhsl_to_srgb(hsl types.HSL) types.RGB {
 		C = k_0 + t*k_1/(1.0-k_2*t)
 	}
 
-	rgb := oklab_to_linear_srgb(Lab{L, C * a_, C * b_})
+	rgb := oklab_to_linear_srgb(lab{L, C * a_, C * b_})
 	rgb_ := types.RGB{
 		R: srgb_transfer_function(rgb.R),
 		G: srgb_transfer_function(rgb.G),
@@ -365,7 +365,7 @@ func Okhsl_to_srgb(hsl types.HSL) types.RGB {
 	return rgb_
 }
 
-func linear_srgb_to_oklab(c types.RGB) Lab {
+func linear_srgb_to_oklab(c types.RGB) lab {
 	l := 0.4122214708*c.R + 0.5363325363*c.G + 0.0514459929*c.B
 	m := 0.2119034982*c.R + 0.6806995451*c.G + 0.1073969566*c.B
 	s := 0.0883024619*c.R + 0.2817188376*c.G + 0.6299787005*c.B
@@ -374,8 +374,8 @@ func linear_srgb_to_oklab(c types.RGB) Lab {
 	m_ := math.Cbrt(m)
 	s_ := math.Cbrt(s)
 
-	return Lab{
-		L: 0.2104542553*l_ + 0.7936177850*m_ - 0.0040720468*s_,
+	return lab{
+		l: 0.2104542553*l_ + 0.7936177850*m_ - 0.0040720468*s_,
 		a: 1.9779984951*l_ - 2.4285922050*m_ + 0.4505937099*s_,
 		b: 0.0259040371*l_ + 0.7827717662*m_ - 0.8086757660*s_,
 	}
@@ -400,13 +400,13 @@ func Srgb_to_okhsl(rgb types.RGB) types.HSL {
 	a_ := lab.a / C
 	b_ := lab.b / C
 
-	L := lab.L
+	L := lab.l
 	h := 0.5 + 0.5*math.Atan2(-lab.b, -lab.a)/math.Pi
 
 	cs := get_Cs(L, a_, b_)
-	C_0 := cs.C_0
-	C_mid := cs.C_mid
-	C_max := cs.C_max
+	C_0 := cs.c_0
+	C_mid := cs.c_mid
+	C_max := cs.c_max
 
 	// Inverse of the interpolation in okhsl_to_srgb:
 
